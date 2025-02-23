9dbae48fc6bcca78aea9f3dd9885f6b73c7de0ef
myjobwell <70344092+myjobwell@users.noreply.github.com>
2025-02-23 18:15:25 -0500

    correcao do deploy - arquivo prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,29 @@
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//   var cachedPrisma: PrismaClient;
+// }
+
+// let prisma: PrismaClient;
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.cachedPrisma) {
+//     global.cachedPrisma = new PrismaClient();
+//   }
+
+//   prisma = global.cachedPrisma;
+// }
+
+// export const db = prisma;
+
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var cachedPrisma: PrismaClient;
-}
+const globalForPrisma = globalThis as unknown as { cachedPrisma?: PrismaClient };
 
-let prisma: PrismaClient;
+export const db = globalForPrisma.cachedPrisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
-  }
+if (process.env.NODE_ENV !== "production") globalForPrisma.cachedPrisma = db;
 
-  prisma = global.cachedPrisma;
-}
 
-export const db = prisma;
